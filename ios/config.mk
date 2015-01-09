d5c45d5dfcdefc8158b29ebab7c7edaa1d82e31d
Eon S. Jeon <esjeon@hyunmu.am>
2015-01-10 00:33:57 +0900

    add sample config.mk for ios/Makefile

---
+++ ios/config.mk
@@ -0,0 +1,14 @@
+
+APP_NAME    := helloworld
+APP_PACKAGE := am.hyunmu.helloworld
+APP_VERSION := 0.0.1
+APP_AUTHOR  := Eon S. Jeon
+
+FRAMEWORKS := UIKit CoreGraphics
+
+SYSROOT := /tmp/iPhoneOS8.1.sdk
+
+USE_ARC := y
+
+CCTOOLS := /tmp/bin
+TARGET_LDID := $(CCTOOLS)/ldid
